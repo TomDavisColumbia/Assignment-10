6af225a76e23f698875f58330b4a61edf74180c8
TomDavisColumbia <td2455@columbia.edu>
2015-04-09 18:13:27 -0400

    I chose two stories from the manhattan category and used the Read More as a drop down for more information on the story

---
+++ dailyNews.js
@@ -0,0 +1,15 @@
+var dailyNews = [  
+    {  
+        "headline":"DOG CUSTODY: Media exec's impotence led to dog custody battle, ex says",
+        "moreInfo":"A media exec alleged problems in getting an erection are front and center in a dog custody battle in a Manhattan court. Chelsea Conrad, the daughter of actor Robert Conrad, says in a sworn affidavit that her ex-boyfriend, Mail Online executive Noah Szubski, initiated the court fight over the ownership of her Doberman because he couldn't control her any more than he could control his performance in bed. “He was unable to achieve and maintain an erection, so (they) were only intimate about four times per year for three years,” Conrad says in a 31-page statement filed Monday in Manhattan Supreme Court. Conrad, 28, says she tried for years to work with Szubski, 32, on his erection problems, but he refused to take doctor-prescribed medication. Instead, she said, he lashed out at her and became increasingly controlling.",
+    },
+    
+]
+
+var story = [  
+    {  
+        "story":"BRIDE AND SEEK: Bronx woman gets married 10 TIMES without divorcing -- including 6 times in one year, officials say",
+        "moreInfo":"A media exec alleged problems in getting an erection are front and center in a dog custody battle in a Manhattan court. Chelsea Conrad, the daughter of actor Robert Conrad, says in a sworn affidavit that her ex-boyfriend, Mail Online executive Noah Szubski, initiated the court fight over the ownership of her Doberman because he couldn't control her any more than he could control his performance in bed. “He was unable to achieve and maintain an erection, so (they) were only intimate about four times per year for three years,” Conrad says in a 31-page statement filed Monday in Manhattan Supreme Court. Conrad, 28, says she tried for years to work with Szubski, 32, on his erection problems, but he refused to take doctor-prescribed medication. Instead, she said, he lashed out at her and became increasingly controlling.",
+    },
+    
+]
