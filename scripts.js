dcd64c6ffd2bebc61e39d4efe67891a47ac201df
TomDavisColumbia <td2455@columbia.edu>
2015-04-12 22:50:23 -0400

    made changes to styles and html

--- scripts.js
+++ scripts.js
@@ -1,59 +1,34 @@
+<!-- I chose this site to replicate in a way: http://www.nydailynews.com/ --!>
+
 for (var i=0; i<dailyNews.length; i++) {
-    var rowData = dailyNews[i];
-    
-    var template = $('<div class="quoteContainer">'+
-                 '<div class="headline">'+rowData.headline+'</div>'+
-                 '<div class="readMore">Read More</div>'+   <!-- here I basically removed the previous Guardian data and replaced it with Daily News stories--!>
-                 '<div class="moreInfo">'+
-                     '<div class="moreInfo">'+rowData.moreInfo+'</div>'+
-                  
-              '</div>'+
-           '</div>');
-           
-           
-    $(template).find(".readMore").on("click", toggleContent);
-    
-    function toggleContent(e) {
-          $quoteContainer = $(this).parent();
-          
-          if ($quoteContainer.hasClass("expanded")) {   //quoteContainer has class .expanded (you can see content), so remove content
-              $quoteContainer.removeClass("expanded"); 
-         } else { //quoteContainer does not have class expanded (you can't see content), so show content!
-             $quoteContainer.addClass("expanded"); <!--Decided to use the same functionality of hiding and dropping more information --!>
-             
-         }
-    }      
-    
-    $('#dataContainer').append(template); 
-           
-}
-
-
-for (var i=0; i<story.length; i++) {
-    var rowData = story[i];
-    
-    var template = $('<div class="quoteContainer">'+
-                 '<div class="story">'+rowData.story+'</div>'+
+    var dataRow = dailyNews[i];
+    	
+    	var	template = $('<div class="quoteContainer">'+
+                 '<div class="headline">'+dataRow.headline+'</div>'+
                  '<div class="readMore">Read More</div>'+
-                 '<div class="moreInfo">'+
-                     '<div class="moreInfo">'+rowData.moreInfo+'</div>'+
-                  
-              '</div>'+
-           '</div>');
+                 '<div class="moreInfo">'+dataRow.moreInfo+'</div>'+ // here I basically removed the previous Guardian data and replaced it with Daily News stories--!>
+             '</div');
            
-           
-    $(template).find(".readMore").on("click", toggleContent);
-    
-    function toggleContent(e) {
-          $quoteContainer = $(this).parent();
-          
-          if ($quoteContainer.hasClass("expanded")) {   //quoteContainer has class .expanded (you can see content), so remove content
-              $quoteContainer.removeClass("expanded"); 
-         } else { //quoteContainer does not have class expanded (you can't see content), so show content!
-             $quoteContainer.addClass("expanded");
-             
-         }
-    }      
     $('#dataContainer').append(template); 
+    
+    	var readMore = template.find(".readMore")
+   		    readMore.on('click', toggleInfo);
+   	
+ }
+
+    
+    function toggleInfo(e){
+          var container = $(e.target).parent();
+          
+           if($(container).hasClass("expanded")){   //quoteContainer has class .expanded (you can see content), so remove content
            
-}
+             $(container).removeClass("expanded"); //<!--Decided to use the same functionality of hiding and dropping more information --!>
+             
+		}else{
+			
+			$(container).addClass('expanded');
+		
+		
+		}
+	
+	}
